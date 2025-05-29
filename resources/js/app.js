da9ab2528d411a65fb99c7e9a24d6973f944b5f3
Fer <fernandosevilla028@gmail.com>
2025-05-29 13:57:43 +0200

    Hecha página inicial. Falta: - Formulario de contacto que envíe Mail directamente. - Alguna mejora visual, animación, cosas así.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,13 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
+import Alpine from 'alpinejs'
+import collapse from '@alpinejs/collapse'
+import focus from '@alpinejs/focus'
+import mask from '@alpinejs/mask'
 
-window.Alpine = Alpine;
+Alpine.plugin(collapse)
+Alpine.plugin(focus)
+Alpine.plugin(mask)
 
-Alpine.start();
+window.Alpine = Alpine
+Alpine.start()
